9f4b59ec9a2e3604e95bd52dff27afbbf8e7bfec
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2020-01-31 11:34:15 -0800

    mona: Add UClamp support for yupik
    Sched Tune is deprecated in 5.4 Kernels and is replaced with
    UClamp. As Android releases has to support earlier Kernel versions
    like 4.14, UClamp isn't made default yet.
    
    Change-Id: If6f4ae0812ae456089273514de58bb0654bb08c7

--- platform/power.mk
+++ platform/power.mk
@@ -14,7 +14,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/power/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+    $(LOCAL_PATH)/power/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # IRQ balance
 PRODUCT_COPY_FILES += \
