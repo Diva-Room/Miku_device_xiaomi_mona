87a778aacbb152dca257d17b1a9c862604a2a4fe
C457 <android.c357@gmail.com>
2022-03-23 15:55:44 +0100

    mona: fingerprint: Add support for extCmd * Xiaomi extends fingerprint_device_t by adding a new method called extCmd used to send vendor specific commands
    Change-Id: Ic94f7b3fd28f7db81695e47f54ce6fa89248bde9

--- platform/fingerprint/fingerprint.h
+++ platform/fingerprint/fingerprint.h
@@ -1,18 +1,9 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ /*
+  * Copyright (C) 2014 The Android Open Source Project
+  *               2022 The LineageOS Project
+  *
+  * SPDX-License-Identifier: Apache-2.0
+  */
 
 #pragma once
 
@@ -258,6 +249,11 @@ typedef struct fingerprint_device {
      *                  or a negative number in case of error, generally from the errno.h set.
      */
     int (*authenticate)(struct fingerprint_device *dev, uint64_t operation_id, uint32_t gid);
+
+    /*
+     * Xiaomi fingerprint extension command.
+     */
+    int (*extCmd)(struct fingerprint_device* dev, int32_t cmd, int32_t param);
 
     /* Reserved for backward binary compatibility */
     void *reserved[4];
