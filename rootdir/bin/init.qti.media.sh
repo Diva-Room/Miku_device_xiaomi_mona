8c3905221ccdf36748a68b53ca681119cab55efe
Kujou Yuko <i@779.moe>
2022-10-29 13:05:55 +0800

    mona: rootdir: Nuke build codename judgment from QTI media init script
     * We have now upgraded to Android 13, it will cause the Netflix
       property to not be set correctly on boot and will not pass the
       certification.
    
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: I58a8ad0943fe520bfb519095fd923d3260c34f6d

--- rootdir/bin/init.qti.media.sh
+++ rootdir/bin/init.qti.media.sh
@@ -42,15 +42,12 @@ fi
 
 target_qssi=`getprop vendor.media.target.qssi`
 target=`getprop ro.board.platform`
-build_codename=`getprop vendor.media.system.build_codename`
 case "$target" in
     "lahaina")
         case "$soc_hwid" in
             475|515)
                 setprop vendor.media.target_variant "_yupik_v0"
-                if [[ $build_codename -eq "11" ]] || [[ $build_codename -eq "12" ]]; then
-                    setprop vendor.netflix.bsp_rev "Q7325-SPY-33758-1"
-                fi
+                setprop vendor.netflix.bsp_rev "Q7325-SPY-33758-1"
                 sku_ver=`cat /sys/devices/platform/soc/aa00000.qcom,vidc/sku_version` 2> /dev/null
                 if [ $sku_ver -eq 1 ]; then
                     setprop vendor.media.target_variant "_yupik_v1"
@@ -58,9 +55,7 @@ case "$target" in
                 ;;
             450)
                 setprop vendor.media.target_variant "_shima_v3"
-                if [ $build_codename -eq "11" ]; then
-                    setprop vendor.netflix.bsp_rev "Q875-32774-1"
-                fi
+                setprop vendor.netflix.bsp_rev "Q875-32774-1"
                 sku_ver=`cat /sys/devices/platform/soc/aa00000.qcom,vidc/sku_version` 2> /dev/null
                 if [ $sku_ver -eq 1 ]; then
                     setprop vendor.media.target_variant "_shima_v1"
@@ -77,9 +72,7 @@ case "$target" in
                 else
                     setprop vendor.media.target_variant "_lahaina"
                 fi
-                if [[ $build_codename -eq "11" ]] || [[ $build_codename -eq "12" ]]; then
-                    setprop vendor.netflix.bsp_rev "Q875-32408-1"
-                fi
+                setprop vendor.netflix.bsp_rev "Q875-32408-1"
                 ;;
         esac
         ;;
@@ -87,15 +80,11 @@ case "$target" in
         case "$soc_hwid" in
             507)
                 setprop vendor.media.target_variant "_blair"
-                if [ $build_codename -eq "11" ]; then
-                    setprop vendor.netflix.bsp_rev "Q4350-32962-1"
-                fi
+                setprop vendor.netflix.bsp_rev "Q4350-32962-1"
                 ;;
             454|472)
                 setprop vendor.media.target_variant "_holi"
-                if [ $build_codename -eq "11" ]; then
-                    setprop vendor.netflix.bsp_rev "Q4350-32962-1"
-                fi
+                setprop vendor.netflix.bsp_rev "Q4350-32962-1"
                 ;;
         esac
         ;;
