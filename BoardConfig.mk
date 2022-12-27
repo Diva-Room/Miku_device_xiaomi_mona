42fd868b45ab43c6ac2cdff4e11af71de00f4360
AmeChanRain <downloadbot007@gmail.com>
2022-12-27 08:21:13 +0000

    mona: Include qcom sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-12-01
 
+# SELinux
+include $(DEVICE_PATH)/platform/sepolicy/sepolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
---
+++ platform/sepolicy/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include device/qcom/sepolicy_vndr/SEPolicy.mk
