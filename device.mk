f66578708938f4917f4d4c2bc940c08851f8af73
AmeChanRain <downloadbot007@gmail.com>
2022-12-29 05:23:26 +0000

    mona: Set udfps overlays Co-authored-by: TheScarastic <warabhishek@gmail.com>

--- device.mk
+++ device.mk
@@ -90,7 +90,8 @@ $(call inherit-product, $(LOCAL_PATH)/platform/nfc.mk)
 $(call inherit-product, $(LOCAL_PATH)/platform/rro_overlay.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
