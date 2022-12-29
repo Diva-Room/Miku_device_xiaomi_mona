982ab1ad48fb4090475f22d6e54d5f3116b3b49f
althafvly <althafvly@gmail.com>
2022-12-29 12:22:40 +0000

    mona: Use randomly generated camera config
    Change-Id: Ib3f6f1526e2e0b7db23bb34c5b15aec1b0d338c2

--- platform/camera.mk
+++ platform/camera.mk
@@ -10,3 +10,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
