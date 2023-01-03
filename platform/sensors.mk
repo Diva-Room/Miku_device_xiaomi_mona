8ae80e49eeda4b9f380d409e21f4a8e47d680f96
AmeChanRain <downloadbot007@gmail.com>
2023-01-03 11:26:26 +0000

    mona: Build libsensor hal

--- platform/sensors.mk
+++ platform/sensors.mk
@@ -6,4 +6,7 @@
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.xiaomi_mona-multihal \
+    libsensorndkbridge \
+    libsensorservice \
+    libsensorservicehidl \
     vendor.qti.hardware.display.mapper@1.1.vendor
