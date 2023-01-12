8561dd01e15b5fd9d54b3fb6583f6e7f0decb14f
AmeChanRain <downloadbot007@gmail.com>
2023-01-12 20:07:22 +0000

    mona: Drop non-existed packages

--- platform/bluetooth.mk
+++ platform/bluetooth.mk
@@ -9,7 +9,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.1-impl \
     com.dsi.ant@1.0.vendor \
-    libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
--- platform/display.mk
+++ platform/display.mk
@@ -11,7 +11,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     gralloc.default \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
