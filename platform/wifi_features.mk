c97f4980661ffad1c259710015d4019fb817756c
Bruno Martins <bgcngm@gmail.com>
2023-01-13 12:57:32 +0000

    mona: Drop QTI WiFi interfaces Change-Id: I91f4042868bbcd18cc16862821f7dc831cb1ca65

--- platform/wifi_features.mk
+++ platform/wifi_features.mk
@@ -11,8 +11,6 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     libwpa_client \
-    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.2.vendor \
     wpa_supplicant \
     wpa_supplicant.conf
 
