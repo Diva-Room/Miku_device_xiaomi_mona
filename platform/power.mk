5ada02c98d17d682aa9c067c5dfe55aa8c4d3042
AmeChanRain <downloadbot007@gmail.com>
2023-03-07 11:56:08 +0000

    mona: Move IRQ balance config to device tree

--- platform/power.mk
+++ platform/power.mk
@@ -15,3 +15,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
+# IRQ balance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
