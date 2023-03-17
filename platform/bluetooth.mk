f5d5fdb90c8dd79a46a4a13a1a75d9fdfb9c7bec
AmeChanRain <downloadbot007@gmail.com>
2023-03-17 11:14:41 +0000

    mona: Build Aptx&Aptx-HD libs from source
    * Google finally introduces these in QPR2

--- platform/bluetooth.mk
+++ platform/bluetooth.mk
@@ -7,6 +7,8 @@
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
+    libaptx_enc \
+    libaptxhd_enc \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
