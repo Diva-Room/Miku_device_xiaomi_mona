73b5b9d231aff9dde11acf463837b1f3deaf5674
AmeChanRain <downloadbot007@gmail.com>
2023-05-06 10:03:50 +0000

    mona: interfaces: Nuke unneeded goodix fingerprint interface
    * It is not needed by anything.

--- platform/fingerprint.mk
+++ platform/fingerprint.mk
@@ -6,5 +6,4 @@
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi_mona \
-    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
