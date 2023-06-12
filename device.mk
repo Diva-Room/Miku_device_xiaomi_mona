06a5ffa0f986eba663cde7fe5b88f383b28a5687
AmeChanRain <downloadbot007@gmail.com>
2023-06-12 12:02:02 +0000

    mona: Declare product & board API level

--- device.mk
+++ device.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(LOCAL_PATH)/platform/vab.mk)
 # ANT+
 $(call inherit-product, $(LOCAL_PATH)/platform/ant.mk)
 
+# API Level
+$(call inherit-product, $(LOCAL_PATH)/platform/api_level.mk)
+
 # Atrace
 PRODUCT_PACKAGES += android.hardware.atrace@1.0-service
 
