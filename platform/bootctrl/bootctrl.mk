d5c9522ae1a3d1adbc8fb8cbaf89b9cdf3ab9f65
AmeChanRain <downloadbot007@gmail.com>
2023-07-07 08:58:46 +0000

    mona: Uprev bootctrl hal to v1.2

--- platform/bootctrl/bootctrl.mk
+++ platform/bootctrl/bootctrl.mk
@@ -5,6 +5,6 @@
 #
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
